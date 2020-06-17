f597276a444f704d794f3168296c26f3109c9b52
unknown <maan@domain.oceansoft>
2020-06-17 09:56:49 +0800

    demo

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+	BrowserRouter as Router,
+	Route
+} from "react-router-dom";
+
+function router() {
+	return ( 
+		<Router>
+			<Route / >
+		</Router>
+	)
+}
+export default router;
